f9db80295d590d953d8b3e6a4bb974d09a0b2fef
System Administrator <root@brobans-MacBook-Pro.local>
2025-01-16 15:03:04 +0200

    added mongo

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-import express from "express"
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+require('dotenv').config()
+const app = express()
+
+app.use(cors());
+
+app.use(express.json())
+
+
+
+mongoose
+  .connect(process.env.MONGO_CONNECTION)
+  .then(() => console.log("Connected!"))
+  .catch(() => {
+    console.log("bad connection");
+  });
+
+  app.get("/", (req, res) => {
+    res.json({ message: "The app is alive!" });
+  });
+
+app.use((req, res) => {
+  res.status(404).json({ response: "your endpoint does not exit" });
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`App was started on port ${process.env.PORT}`);
+  });
+
+
+
+
+
+
+
